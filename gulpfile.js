c5d7d737185e989c187a01be501a4ad06323c8b3
andela-oduyile <oluwatomi.duyile@andela.com>
2017-03-16 16:51:30 +0100

    chore(gulp): added-browserify-gulp-task

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const path = require('path');
+const browserify = require('gulp-browserify');
 const Server = require('karma').Server;
+const rename = require('gulp-rename');
 
 gulp.task('browserSync', () => {
-	browserSync.init({
-		server: {
-			baseDir: 'public'
-		}
-	});
+  browserSync.init({
+    server: {
+      baseDir: 'public'
+    }
+  });
 });
 
 gulp.task('default', ['browserSync'], () => {
-	gulp.watch('public/index.html', browserSync.reload);
-	gulp.watch('public/partials/*.html', browserSync.reload);
-	gulp.watch('public/css/*.css', browserSync.reload);
-	gulp.watch('public/js/*.js', browserSync.reload);
+  gulp.watch('public/index.html', browserSync.reload);
+  gulp.watch('public/partials/*.html', browserSync.reload);
+  gulp.watch('public/css/*.css', browserSync.reload);
+  gulp.watch('public/js/*.js', browserSync.reload);
 });
 
 gulp.task('test', (done) => {
@@ -24,3 +26,11 @@ gulp.task('test', (done) => {
     singleRun: true
   }, done).start();
 });
+
+gulp.task('browserify', () => {
+  gulp.src('./jasmine/spec/inverted-index-test.js')
+  .pipe(browserify())
+  .pipe(rename('bundledTest.js'))
+  .pipe(gulp.dest('jasmine'));
+});
+// ./tests/bundles/bundled-test.js

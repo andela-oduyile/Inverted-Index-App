9c406d77b3de333093e3a98f78285dc01f873446
andela-oduyile <oluwatomi.duyile@andela.com>
2017-03-17 14:52:27 +0100

    add environment checks fror tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const path = require('path');
-const browserify = require('gulp-browserify');
-const Server = require('karma').Server;
-const rename = require('gulp-rename');
+
 
 gulp.task('browserSync', () => {
   browserSync.init({
@@ -20,17 +17,24 @@ gulp.task('default', ['browserSync'], () => {
   gulp.watch('public/js/*.js', browserSync.reload);
 });
 
-gulp.task('test', (done) => {
-  new Server({
-    configFile: path.join(__dirname, 'karma.conf.js'),
-    singleRun: true
-  }, done).start();
-});
+if (process.env.NODE_ENV === 'test') {
+  const path = require('path');
+  const browserify = require('gulp-browserify');
+  const Server = require('karma').Server;
+  const rename = require('gulp-rename');
+  
+  gulp.task('test', (done) => {
+    new Server({
+      configFile: path.join(__dirname, 'karma.conf.js'),
+      singleRun: true
+    }, done).start();
+  });
 
-gulp.task('browserify', () => {
-  gulp.src('./jasmine/spec/inverted-index-test.js')
-  .pipe(browserify())
-  .pipe(rename('bundledTest.js'))
-  .pipe(gulp.dest('jasmine'));
-});
+  gulp.task('browserify', () => {
+    gulp.src('./jasmine/spec/inverted-index-test.js')
+    .pipe(browserify())
+    .pipe(rename('bundledTest.js'))
+    .pipe(gulp.dest('jasmine'));
+  });
+}
 // ./tests/bundles/bundled-test.js
